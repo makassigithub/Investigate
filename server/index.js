5916a6a6c9d5d43feef0e59ce9cfe0c5bc137ec7
Brahima Traore <makassi@BrahimaMac.local>
2023-09-02 22:50:00 -0400

    test CI/CD

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send({ name: 'Brahima' });
+  res.send({ buddy: 'How be' });
 });
 
 app.listen(5000, () => console.log('Server listening at port: 5000'));
