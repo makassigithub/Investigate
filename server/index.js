cdb55ec042d56958cba62230906014c068c8a361
Brahima Traore <makassi@BrahimaMac.local>
2023-09-10 00:09:56 -0400

    configure production routing on client side

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 const passport = require('passport');
+
+const app = express();
 
 require('./models/User');
 require('./services/passport');
@@ -23,6 +25,19 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+if (process.env.NODE_ENV === 'production') {
+  // The dev server does not exist anymore, express will serve both the back and ethe frontEnd
+
+  // 1. instruct express to serve production static contents of the client from the build directory
+  app.use(express.static('client/build'));
+
+  // 2. Instrcut express to send back the index.html file for any route that is that not handle
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 require('./routes/authRoutes')(app);
 require('./routes/paymentRouts')(app);
